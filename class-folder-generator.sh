fb498ea453791e8d16bfbf1e5b1cb128f579d15c
Rosman-Carino <rockycrn@gmail.com>
2026-09-20 12:46:15 -0700

    Adding flag for lecture subdirectories

--- class-folder-generator.sh
+++ class-folder-generator.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Lecture Directory Flag
+create_lecture_subdirs=false
+num_lectures=""
+if [[ "${1:-}" == "-l"]]; then
+    create_lecture_subdirs=true
+    num_lectures="${2:?}"
+    shift 2
+fi 
+
+# User Input Arguments
 num_assignments="$1"
 num_exams="$2"
 num_sections="$3"
@@ -29,6 +39,16 @@ create_subdirectories () {
 
 # Create Default Directories
 mkdir -p Assignments Exams Lectures Lecture-Videos Sections
+
+#Create Subdirectories for Lectures
+if [[ "$create_lecture_subdirs" == true ]]; then
+    echo "Lecture Subdirectory Flag is enabled"
+    echo "Creating $num_lectures lecture subdirectories"
+    create_subdirectories "Lectures" "$num_lectures"
+else
+    echo "Lecture Subdirectory is not enabled"
+fi
+
 create_starter_and_submission_directories "Assignments" "$num_assignments"
 create_starter_and_submission_directories "Sections" "$num_sections"
 create_subdirectories "Exams" "$num_exams"
